ff1f9b403b9e9eb82f442c346554c9d608470fcd
Caner Ates <cnrates@gmail.com>
2024-04-30 14:46:41 +0300

    Initial Commit

---
+++ AdMob-Banner/AdMob_BannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdMob_BannerApp.swift
+//  AdMob-Banner
+//
+//  Created by Caner Ates on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AdMob_BannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
